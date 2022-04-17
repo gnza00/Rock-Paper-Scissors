fd4c1175d1bdaa3d4883cd067415174eda8975a0
gnza00 <gonzaloavila4400@gmail.com>
2022-04-17 12:16:18 -0300

    Create playerSelection and playRound functions

--- script.js
+++ script.js
@@ -6,13 +6,52 @@ function computerPlay() //Returns randomly either Rock, Paper or Scissors
     //console.log(random);
     if(random <=33.3333)
     {
-        return 'Rock';
+        return 'rock';
     } else if(random<=66.6666)
     {
-        return 'Paper';
+        return 'paper';
     }else
     {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 //console.log(computerPlay());
+
+function playerSelection()
+{
+    let selection = prompt("Choose Wisely");
+    return selection.toLowerCase();
+}
+function playRound(computerPlay,playerSelection)
+{
+    let user=computerPlay(),computer=playerSelection();
+    if(user==computer)
+    {
+        return "It's a tie!!";
+    }
+    if(user=='scissors'&&computer=='rock')
+    {
+        return "You lost! :( ";
+    }
+    if(user=='scissors'&&computer=='paper')
+    {
+        return "You won! :D ";
+    }
+    if(user=='rock'&&computer=='paper')
+    {
+        return "You lost! :( ";
+    }
+    if(user=='rock'&&computer=='scissors')
+    {
+        return "You won! :D ";
+    }
+    if(user=='paper'&&computer=='scissors')
+    {
+        return "You lost! :( ";
+    }
+    if(user=='paper'&&computer=='rock')
+    {
+        return "You won! :D ";
+    }
+
+}
