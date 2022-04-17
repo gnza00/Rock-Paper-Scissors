79a19631a5920baaabf876858730456e67ebf2f3
gnza00 <gonzaloavila4400@gmail.com>
2022-04-17 11:47:28 -0300

    Create computerPlay function

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 console.log("Welcome");
 
+function computerPlay() //Returns randomly either Rock, Paper or Scissors
+{
+    let random=Math.random()*100;
+    //console.log(random);
+    if(random <=33.3333)
+    {
+        return 'Rock';
+    } else if(random<=66.6666)
+    {
+        return 'Paper';
+    }else
+    {
+        return 'Scissors';
+    }
+}
+//console.log(computerPlay());
